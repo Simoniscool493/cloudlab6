3a3c3dab88e7c6415ca552f76639639218918f08
simononeill493 <c14444108@mydit.ie>
2016-10-26 14:30:28 +0000

    2

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "hello flask"
+	return "hello flask lmao"
+
+@app.route("/user/<username>")
+def show_user(username):
+	return "Hello user %s" % username
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0",port=3000,debug=True);
